529a7ce1d48bdbb2e6564529b59933994a425bf3
Eliezer516 <ybarretomaterano@gmail.com>
2023-03-02 18:04:53 -0400

    commit inicial

---
+++ main.js
@@ -0,0 +1,31 @@
+import './style.css'
+import 'highlight.js/styles/atom-one-dark.css'
+
+import { CodeJar } from 'codejar'
+import { withLineNumbers } from 'codejar/linenumbers.js'
+
+import hljs from 'highlight.js/lib/core';
+import markdown from 'highlight.js/lib/languages/markdown';
+hljs.registerLanguage('markdown', markdown);
+
+import Split from 'split.js'
+Split(['.split-1', '.split-2'])
+
+import * as showdown from 'showdown'
+
+const editor = document.querySelector('.editor')
+
+const highlight = editor => {
+  editor.textContent = editor.textContent
+  hljs.highlightElement(editor)
+}
+
+const jar = CodeJar(editor, withLineNumbers(highlight), {
+	tab: ' '.repeat(2)
+})
+
+jar.onUpdate(code => {
+	let converter = new showdown.Converter()
+	let htmlConverter = converter.makeHtml(code)
+	document.querySelector('#preview').innerHTML = htmlConverter
+})
